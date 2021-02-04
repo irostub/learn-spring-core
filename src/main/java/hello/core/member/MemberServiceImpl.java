4e4dd8238f249fd46d4f4474a90484318939927b
irostub <irostub@gmail.com>
2021-02-05 03:16:31 +0900

    Add AppConfig.java for dependency injection
    use dependency injection, MemberServiceImpl.java & OrderServiceImpl.java
    only depend on interfaces. and the test code is refactored too.

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -3,7 +3,11 @@ package hello.core.member;
 //관례 : 인터페이스의 구현체가 하나뿐인 경우 Impl로 축약하여 적는다
 public class MemberServiceImpl implements MemberService {
 
-    private final MemberRepository memberRepository = new MemoryMemberRepository();
+    private final MemberRepository memberRepository;
+
+    public MemberServiceImpl(MemberRepository memberRepository) {
+        this.memberRepository = memberRepository;
+    }
 
     @Override
     public void join(Member member) {
