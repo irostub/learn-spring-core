9951fd80d49f2dbef467e4254fdb2168c88a2ff2
irostub <irostub@gmail.com>
2021-02-15 12:10:50 +0900

    Implement application context extend parent find test

---
+++ src/test/java/hello/core/beanfind/ApplicationContextExtendsFindTest.java
@@ -0,0 +1,8 @@
+package hello.core.beanfind;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ApplicationContextExtendsFindTest {
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+
+}
