4a5c2638059195e2965f05921e0d0240a8a03555
irostub <irostub@gmail.com>
2021-02-15 14:16:15 +0900

    Implement test to spring container

--- src/test/java/hello/core/singleton/SingletonTest.java
+++ src/test/java/hello/core/singleton/SingletonTest.java
@@ -2,9 +2,9 @@ package hello.core.singleton;
 
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
 
@@ -38,5 +38,19 @@ public class SingletonTest {
 
         assertThat(instance1).isEqualTo(instance2); //값을 기준으로 같은지 확인
         assertThat(instance1).isSameAs(instance2); //참초를 기준으로 같은지 확인
+    }
+
+    @Test
+    @DisplayName("스프링 컨테이너와 싱글톤")
+    void springContainer() {
+        AnnotationConfigApplicationContext annotationConfigApplicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+        MemberService memberService1 = annotationConfigApplicationContext.getBean(MemberService.class);
+        MemberService memberService2 = annotationConfigApplicationContext.getBean(MemberService.class);
+
+        //참조값이 다른가?
+        System.out.println("memberService1 = " + memberService1);
+        System.out.println("memberService2 = " + memberService2);
+
+        assertThat(memberService1).isSameAs(memberService2);
     }
 }
