d04a7a96f0a24a480758a4d639d6d2a3ffa2bac0
irostub <irostub@gmail.com>
2021-02-20 17:53:22 +0900

    Add annotation @Component to MemberServiceImpl and @Autowired to di

--- src/main/java/hello/core/member/MemberServiceImpl.java
+++ src/main/java/hello/core/member/MemberServiceImpl.java
@@ -1,10 +1,15 @@
 package hello.core.member;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 //관례 : 인터페이스의 구현체가 하나뿐인 경우 Impl로 축약하여 적는다
 public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
+    @Autowired
     public MemberServiceImpl(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
     }
