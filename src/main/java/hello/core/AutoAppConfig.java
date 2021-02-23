b01265f40e8b6332136ebcad78fd1cec3931770e
irostub <irostub@gmail.com>
2021-02-24 03:23:16 +0900

    Add comment to test

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,6 +1,9 @@
 package hello.core;
 
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -23,4 +26,12 @@ import org.springframework.stereotype.Component;
         //이로 볼 때 configuration 은 그와 같은 디렉터리 위치에 두는게 권장된다
 )
 public class AutoAppConfig {
+
+    //01:31:27.304 [main] DEBUG org.springframework.beans.factory.support.DefaultListableBeanFactory - Overriding bean definition for bean 'memoryMemberRepository' with a different definition: replacing [Generic bean: class [hello.core.member.MemoryMemberRepository]; scope=singleton; abstract=false; lazyInit=null; autowireMode=0; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=null; factoryMethodName=null; initMethodName=null; destroyMethodName=null; defined in file [/Users/sdm/Documents/github/learn-spring-core/out/production/classes/hello/core/member/MemoryMemberRepository.class]] with [Root bean: class [null]; scope=; abstract=false; lazyInit=null; autowireMode=3; dependencyCheck=0; autowireCandidate=true; primary=false; factoryBeanName=autoAppConfig; factoryMethodName=memberRepository; initMethodName=null; destroyMethodName=(inferred); defined in hello.core.AutoAppConfig]
+    //위와 같은 로그가 뜨는데 자동 빈 등록과 현재 @Bean 으로 수동 등록 하였을 때 수동등록 빈을 덮어씌워버린 것
+    //이는 의도하지 않은 경우 큰 버그로 돌아올 수 있음. 때문에 springboot 로 실행 시엔 원초부터 오류가 나버리게함.
+    /*@Bean(name = "memoryMemberRepository")
+    MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }*/
 }
