a1d37a01906f83aa1c2aaddac43a5c501e78b814
irostub <irostub@gmail.com>
2021-02-26 16:57:15 +0900

    Add comment override callback method
    best solution: use @PostConstruct and @PreDestroy annotation
    
    second best: use initMethod and destroyMethod in @Bean annotation

--- src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
+++ src/test/java/hello/core/lifecycle/BeanLifeCycleTest.java
@@ -18,7 +18,10 @@ public class BeanLifeCycleTest {
 
     private static class LifeCycleConfig {
 
+        //생명주기 방식 두번째 어노테이션 프로퍼티로 initMethod 와 destroyMethod 를 사용하며 값은 해당 메서드 이름을 문자열로 넣어준다.
         @Bean
+        //(initMethod = "init", destroyMethod = "close") 또 버려짐...세번째 방법 @PostConstruct @PreDestroy 어노테이션을 사용하기 때문
+        //세번째 방법은 편하고 좋고 표준인데, 외부 라이브러리엔 붙일 수 없으므로 외부 라이브러리의 생성과 소멸을 적용하려면 두번째 방법을 쓰면된다.(initMethod 와 destroyMethod)
         public NetworkClient networkClient() {
             NetworkClient networkClient = new NetworkClient();
             networkClient.setUrl("hello network client setting");
--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,12 +1,27 @@
 package hello.core.lifecycle;
 
+import org.springframework.beans.factory.DisposableBean;
+import org.springframework.beans.factory.InitializingBean;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+/**
+ * 빈 생명주기 콜백에 대해서
+ * implements InitializingBean, DisposableBean 를 사용해서
+ * 스프링에서 오버라이드 하여 생명주기를 콜백받는 방식은 옛날 방식이다.
+ * 너무 스프링에 의존적이므로, 그리고 코드를 고칠 수 없는 외부 라이브러리 사용 시에 이 방법으로 생명주기를 관리할 방법이 없다.
+ */
 public class NetworkClient {
     private String url;
 
     public NetworkClient() {
         System.out.println("생성자 호출 : url = " + url);
+        /*
+        afterPropertiesSet 에서 실행합니다. 생명주기 콜백함수
         connect();
         call("초기화 연결 메세지");
+        */
     }
 
     public void setUrl(String url) {
@@ -23,5 +38,22 @@ public class NetworkClient {
 
     public void disconnect() {
         System.out.println("close : " + url);
+    }
+
+    //InitalizingBean 인터페이스로부터,
+    //@Override 고전방식
+    @PostConstruct
+    public void init() {
+        System.out.println("NetworkClient.afterPropertiesSet");
+        connect();
+        call("초기화 연결 메세지");
+    }
+
+    //DisposableBean 인터페이스로부터
+    //@Override 고전방식
+    @PreDestroy
+    public void close() {
+        System.out.println("NetworkClient.destroy");
+        disconnect();
     }
 }
