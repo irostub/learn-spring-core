6dd442b3b08b6f7b6f437ebcc74f738f9ce26ec5
irostub <irostub@gmail.com>
2021-02-26 18:59:33 +0900

    Implement singleton bean test

---
+++ src/test/java/hello/core/scope/SingletonTest.java
@@ -0,0 +1,39 @@
+package hello.core.scope;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.context.annotation.Scope;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class SingletonTest {
+
+    @Test
+    @DisplayName("싱글톤 빈은 정말 하나의 빈을 공유하는가?")
+    void singletonBeanFind() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonBean.class);
+
+        SingletonBean bean1 = ac.getBean(SingletonBean.class);
+        SingletonBean bean2 = ac.getBean(SingletonBean.class);
+        System.out.println("bean1 = " + bean1);
+        System.out.println("bean2 = " + bean2);
+        assertThat(bean1).isSameAs(bean2);
+    }
+
+    @Scope("singleton")
+    static class SingletonBean {
+        @PostConstruct
+        public void init() {
+            System.out.println("SingletonBean.init");
+        }
+
+        @PreDestroy
+        public void close() {
+            System.out.println("SingletonBean.close");
+        }
+    }
+}
