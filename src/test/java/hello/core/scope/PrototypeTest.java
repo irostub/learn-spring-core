b37220f40969f8ac04904c467927f3562b7baf5b
irostub <irostub@gmail.com>
2021-02-28 11:03:42 +0900

    Use prototype bean with singleton bean
    how to return new prototype bean in singleton bean.

--- src/test/java/hello/core/scope/PrototypeTest.java
+++ src/test/java/hello/core/scope/PrototypeTest.java
@@ -1,13 +1,19 @@
 package hello.core.scope;
 
+import lombok.RequiredArgsConstructor;
+import lombok.ToString;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.ObjectProvider;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Scope;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
+import javax.inject.Provider;
 
+import static org.assertj.core.api.Assertions.*;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PrototypeTest {
@@ -27,11 +33,42 @@ public class PrototypeTest {
         bean2.close();
     }
 
+    @Test
+    @DisplayName("싱글톤 클라이언트에 프로토타입 빈을 사용")
+    void singletonClientUsePrototype() {
+        AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SingletonClient.class, PrototypeBean.class);
+
+        SingletonClient bean1 = ac.getBean(SingletonClient.class);
+        SingletonClient bean2 = ac.getBean(SingletonClient.class);
+
+        assertThat(bean1).isSameAs(bean2);
+        assertThat(bean1.prototypeBeanProvider.get()).isNotSameAs(bean2.prototypeBeanProvider.get());
+    }
+
+    @RequiredArgsConstructor
+    static class SingletonClient {
+        @Autowired
+        //private ObjectProvider<PrototypeBean> prototypeBean; ObjectProvider 를 쓸 경우 스프링에 의존하는 대신 기능이 더 많음. getObject() 로 프토로타입 빈 꺼내온다. DL 이라고 하고 dependency lookup 이다.
+        private Provider<PrototypeBean> prototypeBeanProvider; //javax 에서 java 표준임을 확인할 수 있고 java 표준도 Provider 를 지원한다는 것을 알 수 있다. 진짜 get() 기능만 있는 깔끔함.
+
+        int logic() {
+            int count = prototypeBeanProvider.get().increaseCount();
+            return count;
+        }
+    }
+
     @Scope("prototype")
     static class PrototypeBean {
+        private int count;
+
+        int increaseCount() {
+            count += 1;
+            return count;
+        }
+
         @PostConstruct
         public void init() {
-            System.out.println("prototype.init");
+            System.out.println("prototype.init" + this);
         }
 
         @PreDestroy
