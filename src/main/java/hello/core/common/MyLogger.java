84a318f5ee72430f2e7413ac8c10ab629bc7cf7a
irostub <irostub@gmail.com>
2021-02-28 11:04:21 +0900

    Implement request scope logger class

---
+++ src/main/java/hello/core/common/MyLogger.java
@@ -0,0 +1,38 @@
+package hello.core.common;
+
+import org.springframework.context.annotation.Scope;
+import org.springframework.context.annotation.ScopedProxyMode;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.PostConstruct;
+import javax.annotation.PreDestroy;
+import java.util.UUID;
+
+@Component
+//프록시 모드를 설정함으로써 해당 타겟 클래스를 상속받은 가짜 객체가 최초에 주입이된다
+@Scope(value = "request", proxyMode = ScopedProxyMode.TARGET_CLASS)
+public class MyLogger {
+
+    private String uuid;
+    private String requestURL;
+
+    public void setRequestURL(String requestURL) {
+        this.requestURL = requestURL;
+    }
+
+    public void log(String message) {
+        System.out.println("[" + uuid + "]" + " [" + requestURL + "] " + message);
+    }
+
+    @PostConstruct
+    public void init() {
+        uuid = UUID.randomUUID().toString();
+        System.out.println("[" + uuid + "] request scope bean create : " + this);
+    }
+
+    @PreDestroy
+    public void close() {
+        System.out.println("[" + uuid + "] request scope bean close : " + this);
+    }
+}
+
