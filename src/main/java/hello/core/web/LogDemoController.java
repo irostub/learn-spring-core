16abd07737f8abb71cea3d95c039ca908d455cdb
irostub <irostub@gmail.com>
2021-02-28 11:04:58 +0900

    Implement controller and service with request scope logger class

---
+++ src/main/java/hello/core/web/LogDemoController.java
@@ -0,0 +1,27 @@
+package hello.core.web;
+
+import hello.core.common.MyLogger;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+@RequiredArgsConstructor
+public class LogDemoController {
+
+    private final LogDemoService logDemoService;
+    private final MyLogger myLogger;
+
+    @RequestMapping("/")
+    @ResponseBody
+    public String logDemo(HttpServletRequest httpServletRequest) {
+        String s = httpServletRequest.getRequestURL().toString();
+
+        myLogger.log("controller test");
+        logDemoService.logic("testId");
+        return "ok";
+    }
+}
